18e421581b48ad08d57f53ac30e186acd997d05a
Cameron Gray <cameron.gray@senecacollege.ca>
2021-06-30 10:14:33 -0400

    Deploy website - based on 838414f83f5fe943bb64dfcb04970845836b4abb

---
+++ assets/js/30a24c52.c5d54511.js
@@ -0,0 +1,1 @@
+(self.webpackChunkipc_144=self.webpackChunkipc_144||[]).push([[453],{516:function(l){"use strict";l.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/blog/tags/hello"}')}}]);
